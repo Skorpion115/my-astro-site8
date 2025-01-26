664c10698db8d03e5e0d24a991772c19eb8c0310
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2025-01-26 05:03:36 +0100

    MarkdownPostLayout.astro added

--- astronautical-ascension/astro.config.mjs
+++ astronautical-ascension/astro.config.mjs
@@ -1,5 +1,50 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+
+
+// Convert import.meta.url to __dirname and __filename
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: import.meta.env.SITE_URL,
+
+  devToolbar: {
+    enabled: false,
+  },
+  
+  build: {
+    format: "directory", // Erzeugt `page/index.html` statt `page.html`
+    // sourcemap: true, // Aktiviert Source Maps in Vite
+    //chunkSizeWarningLimit: 3420 // Setze hier deine bevorzugte Grenze ein
+  },
+
+  vite: {
+    resolve: {
+      alias: {
+        "@scripts": resolve(__dirname, "public/scripts")
+      }
+      // Weitere Vite-spezifische Konfiguration hier hinzufügen, falls nötig
+    }
+  },
+    // Project root directory
+  root: ".",
+
+  outDir: "dist", // Ensure this is correctly set
+  
+  publicDir: "public",
+  
+  // site: "https://www.musicstudio-ziebart.de",
+
+  trailingSlash: "always",
+
+  markdown: {
+    syntaxHighlight: "shiki", // Optional: Enables syntax highlighting with Shiki
+    gfm: true, // Optional: Enables GitHub Flavored Markdown
+    smartypants: true, // Optional: Smart typographic replacements
+  },
+  
+});
