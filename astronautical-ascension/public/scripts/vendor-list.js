df9215484be69ab8a05f50ebecb396def99df886
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2025-02-08 20:49:46 +0100

    neue vendor-list.js added

---
+++ astronautical-ascension/public/scripts/vendor-list.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let script = document.createElement("script");
+    script.src = "https://delivery.consentmanager.net/delivery/vendorlist.php?cdid=b018a967f10b6&l=de&h=musicstudio-ziebart.de";
+    script.type = "text/javascript";
+    script.async = true;
+    document.body.appendChild(script);
+  });
+  
