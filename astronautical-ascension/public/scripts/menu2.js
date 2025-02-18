6035c40bbed4cdc436734bf6044bb3f787b1c64c
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2025-02-18 17:11:45 +0100

    menu2.js changed

--- astronautical-ascension/public/scripts/menu2.js
+++ astronautical-ascension/public/scripts/menu2.js
@@ -1,8 +1,31 @@
+/*
 document.addEventListener("DOMContentLoaded", function() {
     document.querySelectorAll(".menu-button").forEach(button => {
       button.addEventListener("click", function() {
         this.nextElementSibling.classList.toggle("open");
       });
     });
-  });
+  }); */
   
+
+// Beispiel in menu2.js
+/*
+document.addEventListener("DOMContentLoaded", function() {
+  const menuButton = document.getElementById("menuButton");
+  menuButton.addEventListener("click", function() {
+    // Code für das Aufklappen des Menüs
+    alert("Menü aufgeklappt!");
+  });
+}); */
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  const vendorList = document.getElementById("cmpvendorlist");
+  
+  if (vendorList) {
+    vendorList.addEventListener("click", function() {
+      // Deine Logik zum Aufklappen des Menüs oder der Liste
+      alert("Vendorlist wurde angeklickt!");
+    });
+  }
+});
