20f49d03c2d82b214090038ffdfdfa06f64b05ea
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2025-02-19 20:16:54 +0100

    vendor-list.js added

--- astronautical-ascension/public/scripts/anbieter-list.js
+++ astronautical-ascension/public/scripts/anbieter-list.js
@@ -1,6 +1,6 @@
 // Dynamisch das Script für die Anbieter-Liste laden
 const script = document.createElement("script");
-script.src = "https://delivery.consentmanager.net/delivery/vendorlist.php?cdid=b018a967f10b6&l=automatic";
+script.src = "https://delivery.consentmanager.net/delivery/vendorlist.php?cdid=b018a967f10b6&l=de&h=musicstudio-ziebart.de";
 script.type = "text/javascript";
 script.async = true;
 document.body.appendChild(script);
