d234f3ec094ce92ca767969d67017f326d101a7e
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2025-02-19 14:46:00 +0100

    anbieter.astro changed

--- astronautical-ascension/public/scripts/menu2.js
+++ astronautical-ascension/public/scripts/menu2.js
@@ -1,3 +1,4 @@
+/*
 document.addEventListener("DOMContentLoaded", function () {
   const vendorList = document.getElementById("cmpvendorlist");
 
@@ -12,6 +13,28 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
   }
+}); */
+
+document.addEventListener("DOMContentLoaded", function () {
+  const vendorList1 = document.getElementById("cmpvendorlist");
+  const vendorList2 = document.querySelector(".cmpvendorlistb018a967f10b6");
+
+  function toggleMenu() {
+    console.log("Vendorlist wurde angeklickt!");
+    const menu = document.getElementById("menu");
+    if (menu) {
+      menu.style.display = menu.style.display === "none" ? "block" : "none";
+    }
+  }
+
+  if (vendorList1) {
+    vendorList1.addEventListener("click", toggleMenu);
+  }
+
+  if (vendorList2) {
+    vendorList2.addEventListener("click", toggleMenu);
+  }
 });
+
 
 
