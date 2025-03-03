cc2ee98871a5a19d5a2e01c6bc02c2abf5eaa606
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2025-03-03 09:33:00 +0100

    anbieter.astro delete

--- astronautical-ascension/public/scripts/debug.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const scripts = document.querySelectorAll("script[nonce]");
-  scripts.forEach((script, index) => {
-    console.log(`Script ${index + 1} nonce:`, script.nonce || "Nicht vorhanden");
-  });
-});
-
