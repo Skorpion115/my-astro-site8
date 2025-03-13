948c55a07872670233e34216c5b3c2d5ee2e2ccb
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2025-03-13 18:41:38 +0100

    andere delete-gacookies.js

--- astronautical-ascension/public/scripts/delete-gacookies.js
+++ astronautical-ascension/public/scripts/delete-gacookies.js
@@ -1,6 +1,6 @@
-(function (w, d) {
+(function () {
     function deleteGACookies() {
-      document.cookie = "_ga=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=" + location.hostname + ";";
+      document.cookie = "_ga=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
       document.cookie = "_ga_CXBFDX52C7=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
       document.cookie = "test_cookie=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
       console.log("Google Analytics Cookies wurden gelöscht.");
@@ -8,32 +8,29 @@
   
     function onIubendaLoaded() {
       if (typeof _iub !== "undefined" && _iub.cs && _iub.cs.api) {
-        _iub.cs.api.onConsentGiven(function () {
-          console.log("[iubenda] Einwilligung erteilt.");
-        });
+        console.log("[iubenda] Consent-Manager geladen.");
   
         _iub.cs.api.onConsentReject(function () {
-          console.log("[iubenda.js] Analytics abgelehnt – lösche Cookies...");
+          console.log("[iubenda] Analytics abgelehnt – lösche GA-Cookies...");
           deleteGACookies();
         });
-      } else {
-        console.log("Warte auf Iubenda...");
-        setTimeout(onIubendaReady, 500);
       }
     }
   
-    function onIubendaReady() {
+    function waitForIubenda(retries) {
       if (typeof _iub !== "undefined" && _iub.cs && _iub.cs.api) {
-        onIubendaReady();
+        onIubendaLoaded();
+      } else if (retries > 0) {
+        setTimeout(function () {
+          waitForIubenda(retries - 1);
+        }, 500);
       } else {
-        setTimeout(onIubendaReady, 500);
+        console.warn("[iubenda] Konnte Consent-Manager nicht laden.");
       }
     }
   
-    if (document.readyState === "complete") {
-      onIubendaReady();
-    } else {
-      w.addEventListener("load", onIubendaReady);
-    }
-  })(window, document);
+    document.addEventListener("DOMContentLoaded", function () {
+      waitForIubenda(10); // Maximal 10 Versuche (5 Sekunden Wartezeit)
+    });
+  })();
   
