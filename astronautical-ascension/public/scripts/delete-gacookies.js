3f7914d4443a6b581bc060c57fe5f4d0b8992792
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2025-03-13 18:28:10 +0100

    neues script

--- astronautical-ascension/public/scripts/delete-gacookies.js
+++ astronautical-ascension/public/scripts/delete-gacookies.js
@@ -1,31 +1,39 @@
-(function () {
+(function (w, d) {
     function deleteGACookies() {
-      document.cookie = "_ga=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-      document.cookie = "_ga_CXBFDX52C7=; expires=Thu, 01 Jan 1970 00:01:00 UTC; path=/;";
+      document.cookie = "_ga=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=" + location.hostname + ";";
+      document.cookie = "_ga_CXBFDX52C7=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
       document.cookie = "test_cookie=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+      console.log("Google Analytics Cookies wurden gelöscht.");
     }
   
-    function iubendaEventListener() {
-      if (typeof _iub !== "undefined" && _iub.cons) {
-        if (!_iub.cons.consent.analytics) {
-          console.log("Consent für Analytics wurde abgelehnt. Lösche GA-Cookies...");
+    function onIubendaLoaded() {
+      if (typeof _iub !== "undefined" && _iub.cs && _iub.cs.api) {
+        _iub.cs.api.onConsentGiven(function () {
+          console.log("[iubenda] Einwilligung erteilt.");
+        });
+  
+        _iub.cs.api.onConsentReject(function () {
+          console.log("[iubenda.js] Analytics abgelehnt – lösche Cookies...");
           deleteGACookies();
-        }
+        });
+      } else {
+        console.log("Warte auf Iubenda...");
+        setTimeout(onIubendaReady, 500);
       }
     }
   
-    // Warte auf das Laden von iubenda
-    document.addEventListener("DOMContentLoaded", function () {
-      if (typeof _iub !== "undefined") {
-        _iub.csConfiguration.callbacks.onPreferenceExpressed = function (acceptance) {
-          if (!acceptance.analytics) {
-            console.log("Consent für Analytics wurde abgelehnt. Lösche GA-Cookies...");
-            deleteGACookies();
-          }
-        };
+    function onIubendaReady() {
+      if (typeof _iub !== "undefined" && _iub.cs && _iub.cs.api) {
+        onIubendaReady();
       } else {
-        console.warn("iubenda nicht geladen!");
+        setTimeout(onIubendaReady, 500);
       }
-    });
-  })();
+    }
+  
+    if (document.readyState === "complete") {
+      onIubendaReady();
+    } else {
+      w.addEventListener("load", onIubendaReady);
+    }
+  })(window, document);
   
